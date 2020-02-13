6dcdfc46ae7bcedfae2c4721c58b7149a74d67c4
Milimir Aksić <milimir.aksic@gmail.com>
2020-02-13 13:09:48 +0100

    Refactoring i dodavanje komentara

--- src/controllers/ContactPersonController.java
+++ src/controllers/ContactPersonController.java
@@ -8,13 +8,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.StringConverter;
+import javafx.util.converter.DoubleStringConverter;
+import javafx.util.converter.FormatStringConverter;
 import model.DTO.ContactPerson;
 import model.DTO.Sponsor;
 import model.DTO.SponsorContactPerson;
 import model.DAO.DAOFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,13 +27,13 @@ public class ContactPersonController {
     private Label sponsorlabel;
 
     @FXML
-    private TextField nameLabel;
+    private TextField nameTextField;
 
     @FXML
-    private TextField lastNameLabel;
+    private TextField lastNameTextField;
 
     @FXML
-    private TextField phoneNumberLabel;
+    private TextField phoneNumberTextField;
 
     @FXML
     private TableView<ContactPerson> contactTable;
@@ -40,18 +43,18 @@ public class ContactPersonController {
 
     @FXML
     void addContactPerson(ActionEvent event) {
-        if(!nameLabel.getText().isEmpty() && !lastNameLabel.getText().isEmpty() && !phoneNumberLabel.getText().isEmpty()){
+        if(!nameTextField.getText().isEmpty() && !lastNameTextField.getText().isEmpty() && !phoneNumberTextField.getText().isEmpty()){
             ContactPerson contactPerson=new ContactPerson();
-            contactPerson.setName(nameLabel.getText());
-            contactPerson.setSurname(lastNameLabel.getText());
-            contactPerson.setPhoneNumber(phoneNumberLabel.getText());
+            contactPerson.setName(nameTextField.getText());
+            contactPerson.setSurname(lastNameTextField.getText());
+            contactPerson.setPhoneNumber(phoneNumberTextField.getText());
             DAOFactory.getDAOFactory().getContactPersonDAO().insert(contactPerson);
             DAOFactory.getDAOFactory().getsponsorContractPersonDAO().insert(new SponsorContactPerson(sponsor,contactPerson));
             reloadTable();
         }
     }
     @FXML
-    void deleteContact(ActionEvent event) {
+    void deleteContact(ActionEvent event) { //brise kontakt osobu iz tabele i baze
        if(!contactTable.getSelectionModel().isEmpty()){
            ContactPerson contactPerson=contactTable.getSelectionModel().getSelectedItem();
            DAOFactory.getDAOFactory().getContactPersonDAO().delete(contactPerson);
@@ -61,7 +64,7 @@ public class ContactPersonController {
     }
 
     @FXML
-    void close(ActionEvent event){
+    void close(ActionEvent event){ //zatvara formu -- sakriva je i cisti polja
         contactTable.getSelectionModel().clearSelection();
         contactTable.getScene().getWindow().hide();
     }
@@ -70,15 +73,16 @@ public class ContactPersonController {
         contactTable.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("name"));
         contactTable.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("surname"));
         contactTable.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
+
     }
 
-    public void setSponsor(Sponsor sponsor){
+    public void setSponsor(Sponsor sponsor){    //postavlja podatke o kontakt osobama za specificnog sponzora
         this.sponsor=sponsor;
-        System.out.println(sponsor);
+
         sponsorlabel.setText(sponsor.getName());
         reloadTable();
     }
-    private void reloadTable(){
+    private void reloadTable(){ //ponovno uvitava podatke iz baze
         List<SponsorContactPerson> sponsorContactPersonList= DAOFactory.getDAOFactory().getsponsorContractPersonDAO().sponsorContactPersons();
         List<ContactPerson> contactPersonList =sponsorContactPersonList.stream()
                 .filter(e->e.getSponsor().equals(sponsor))
--- src/controllers/PaymentController.java
+++ src/controllers/PaymentController.java
@@ -4,17 +4,13 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.util.converter.DoubleStringConverter;
 import model.DAO.DAOFactory;
-import model.DTO.ContactPerson;
 import model.DTO.Payment;
 import model.DTO.Sponsor;
-import model.DTO.SponsorContactPerson;
-
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -40,16 +36,32 @@ public class PaymentController {
     private Sponsor sponsor;
     private ObservableList<Payment> paymentObservableList;
     @FXML
-    void addPayment(ActionEvent event) {
+    void addPayment(ActionEvent event) { // dodaje trenutno unesene podatke u novu uplatu
         if(paymentDatePicker.getValue()!=null && expirationDatePicker.getValue()!=null && !paymentAmountField.getText().isEmpty()){
             Payment payment=new Payment();
-            payment.setPaymentDate(Timestamp.valueOf(paymentDatePicker.getValue().atStartOfDay()));
-            payment.setExpirationDate(Timestamp.valueOf(expirationDatePicker.getValue().atStartOfDay()));
-            payment.setAmount(Double.parseDouble(paymentAmountField.getText()));
-            payment.setSponsor(sponsor);
-            DAOFactory.getDAOFactory().geTPaymentDAO().insert(payment);
+
+                loadPayment(payment);
+                DAOFactory.getDAOFactory().geTPaymentDAO().insert(payment);
+
             reloadTable();
         }
+    }
+    @FXML
+     void updatePayment(ActionEvent event) { //sacuvava trenutno unesene podatke u trenutno selektovanu uplatu
+        if(!paymentTable.getSelectionModel().isEmpty()){
+            Payment payment=paymentTable.getSelectionModel().getSelectedItem();
+            loadPayment(payment);
+            DAOFactory.getDAOFactory().geTPaymentDAO().update(payment);
+            reloadTable();
+        }
+    }
+    @FXML
+    void selectionChange(MouseEvent event) { // setuje podatke o uplati na gui, za potrebe promjene
+        Payment payment=paymentTable.getSelectionModel().getSelectedItem();
+        paymentDatePicker.setValue(payment.getPaymentDate().toLocalDateTime().toLocalDate());
+        expirationDatePicker.setValue(payment.getExpirationDate().toLocalDateTime().toLocalDate());
+        paymentAmountField.setText(""+payment.getAmount());
+
     }
 
     @FXML
@@ -59,23 +71,29 @@ public class PaymentController {
     }
 
 
-    public void setSponsor(Sponsor sponsor){
+    public void setSponsor(Sponsor sponsor){    //sluzi za prihvatanje informacija od drugih kontorlera
         this.sponsor=sponsor;
         sponsorNameLabel.setText(sponsor.getName());
         reloadTable();
     }
-    private void reloadTable(){
+    private void reloadTable(){ // pomocna metoda za ucitavanje tabele
         List<Payment> paymentList= DAOFactory.getDAOFactory().geTPaymentDAO().payments().stream()
                 .filter(e->e.getSponsor().equals(sponsor)).collect(Collectors.toList());
         paymentObservableList= FXCollections.observableList(paymentList);
         paymentTable.setItems(paymentObservableList);
     }
-
+    private void loadPayment(Payment payment) { //ucitava uplatu sa gui formi
+        payment.setPaymentDate(Timestamp.valueOf(paymentDatePicker.getValue().atStartOfDay()));
+        payment.setExpirationDate(Timestamp.valueOf(expirationDatePicker.getValue().atStartOfDay()));
+        payment.setAmount(Double.parseDouble(paymentAmountField.getText()));
+        payment.setSponsor(sponsor);
+    }
     @FXML
     void initialize() {
         paymentTable.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("paymentDate"));
         paymentTable.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("expirationDate"));
         paymentTable.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("amount"));
+        paymentAmountField.setTextFormatter(new TextFormatter<>(new DoubleStringConverter()));
 
     }
 }
