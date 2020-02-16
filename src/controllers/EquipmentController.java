cbb1af6ed78a376fb6ac815c3c4ba54b1f5167bc
StefanLekanic <rlekanic@hotmail.com>
2020-02-16 13:12:26 +0100

    Equipment i Fines controller

---
+++ src/controllers/EquipmentController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class EquipmentController {
+
+    @FXML
+    private TextField typeTxtField;
+
+    @FXML
+    private Label playerLabel;
+
+    @FXML
+    private TableView<?> equipmentTable;
+
+    @FXML
+    private TextField numberTxtField;
+
+    @FXML
+    private TextField codeTxtField;
+
+    @FXML
+    void addEquipment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteEquipment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void close(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+}
---
+++ src/controllers/FinesController.java
@@ -0,0 +1,70 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import model.DAO.DAOFactory;
+
+public class FinesController {
+
+    @FXML
+    private TableView<?> finesTable;
+
+    @FXML
+    private Label playerLabel;
+
+    @FXML
+    private TextField suspensionTxtField;
+
+    @FXML
+    private RadioButton redCardButton;
+
+    @FXML
+    private RadioButton yellowCardButton;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private TextField descriptionTxtField;
+
+    @FXML
+    private TextField priceTxtField;
+
+    @FXML
+    private ToggleGroup card;
+
+    @FXML
+    void bdff42(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ff1f1f(ActionEvent event) {
+
+    }
+
+    @FXML
+    void add(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void close(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+    }
+}
