03142b93fb225d386d4cee86a5cfafd909a31a0b
StefanLekanic <rlekanic@hotmail.com>
2020-02-17 18:22:06 +0100

    EquipmentControllerUpdate

--- src/controllers/EquipmentController.java
+++ src/controllers/EquipmentController.java
@@ -4,9 +4,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.DAO.DAOFactory;
 import model.DTO.ContactPerson;
@@ -80,11 +85,28 @@ public class EquipmentController {
 
     @FXML
     void close(ActionEvent event) {
-
+        equipmentTable.getSelectionModel().clearSelection();
+        equipmentTable.getScene().getWindow().hide();
     }
 
     @FXML
-    void initialize() {
+    void initialize() throws  Exception{
+        equipmentTable.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("type"));
+        equipmentTable.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("jerseyNumber"));
+        equipmentTable.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("code"));
+
+        FXMLLoader loader=new FXMLLoader(this.getClass().getResource("../view/alert.fxml"));
+        Parent root=loader.load();
+        alertController=loader.getController();
+        alertStage=new Stage();
+        alertStage.setScene(new Scene(root));
+        loader = new FXMLLoader(this.getClass().getResource("../view/decision.fxml"));
+        root = loader.load();
+        decisionController = loader.getController();
+
+        decisionStage = new Stage();
+        decisionStage.initModality(Modality.APPLICATION_MODAL);
+        decisionStage.setScene(new Scene(root));
 
     }
 
@@ -94,5 +116,11 @@ public class EquipmentController {
                 .filter(e->e.getPerson().equals(player)).collect(Collectors.toList());
         equipmentObservableList= FXCollections.observableList(playerEquipmentList);
         equipmentTable.setItems(equipmentObservableList);
+    }
+
+    public void setPlayer(Person player){
+        this.player = player;
+        playerLabel.setText(player.getName());
+        reloadTable();
     }
 }
